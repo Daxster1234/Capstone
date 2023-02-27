e818ff610da8803bc8a0c9dcdf34bf7517b6f149
Dax Meredith <dax.meredith@gmail.com>
2023-02-27 14:20:45 -0500

    Commented out router.hooks

--- index.js
+++ index.js
@@ -24,39 +24,39 @@ document.querySelector(".fa-bars").addEventListener("click", () => {
 });
 }
 
-router.hooks({
-  before: (done, params) => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home"; // Add a switch case statement to handle multiple routes
-    // Add a switch case statement to handle multiple routes
-    switch (view) {
-      case "Home":
-        axios
-          .get(
-            //`api link goes here`
-          )
-          .then(response => {
+// router.hooks({
+//   before: (done, params) => {
+//     const view =
+//       params && params.data && params.data.view
+//         ? capitalize(params.data.view)
+//         : "Home"; // Add a switch case statement to handle multiple routes
+//     // Add a switch case statement to handle multiple routes
+//     switch (view) {
+//       case "Home":
+//         axios
+//           .get(
+//             //`api link goes here`
+//           )
+//           .then(response => {
 
 
-            done();
-          });
-        break;
-      default:
-        done();
-    }
+//             done();
+//           });
+//         break;
+//       default:
+//         done();
+//     }
 
-  },
-  already: params => {
-    const view =
-      params && params.data && params.data.view
-        ? capitalize(params.data.view)
-        : "Home";
+//   },
+//   already: params => {
+//     const view =
+//       params && params.data && params.data.view
+//         ? capitalize(params.data.view)
+//         : "Home";
 
-    render(store[view]);
-  }
-});
+//     render(store[view]);
+//   }
+// });
 
 router
   .on({
