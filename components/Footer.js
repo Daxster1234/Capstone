d8ba851d22c14e3fdb4361c269da773f703ee7b4
Dax Meredith <dax.meredith@gmail.com>
2023-03-21 21:58:26 -0400

    MVP

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,6 @@ import html from "html-literal";
 export default () => html`
 <footer>
 <ul>
-  <li><a href="https://www.instagram.com/machete.exe/" target="_blank">Machete</a></li>
+  <li><a href="https://www.instagram.com/machete.exe/" target="_blank">Machete on Instagram</a></li>
 </ul>
 </footer>`;
--- components/views/Home.js
+++ components/views/Home.js
@@ -4,4 +4,33 @@ import html from "html-literal";
 export default () => html`
 <div>
 <iframe id="mainVid"width="896" height="504" src="https://www.youtube.com/embed/Lj1yQsHWjyk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-</div>`
+</div>
+<div id='spotify'>
+  <p>Thank you for coming to check out Machete! We'd love to see you at a show some day. We don't have any music to share right now, but we'd
+    like to recommend this podcast on the history of punk music.
+  </p>
+</div>
+<div id="embed-iframe"></div>
+<ul id="episodes">
+<li>
+    <button data-spotify-id="spotify:episode:66jnO3vrl8Aji20pkl1Y7w">
+      Setting the Scene
+    </button>
+  </li>
+  <li>
+    <button data-spotify-id="spotify:episode:0rlEOf34urFJrkhMyLRGRk">
+      MC5 and The Stooges
+    </button>
+  </li>
+  <li>
+    <button data-spotify-id="spotify:episode:2O10nZiLleihrnj9GLXKmy">
+      The Ramones
+    </button>
+  </li>
+  <li>
+    <button data-spotify-id="spotify:episode:7kXncSVpIoYtMtkbQlg5eE">
+      The Talking Heads
+    </button>
+  </li>
+</ul>
+`
